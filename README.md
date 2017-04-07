75863d87720e2af4eb185ad4a3836d2f8c25369d
Eric Jiang <ericjiang97@gmail.com>
2017-04-07 11:28:30 +1000

    updated microservice code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # micro-aushol
-A Micro-service for Australian Holidays :aus
+A Micro-service for Australian Holidays 
+
+This was inspired by the micro-service built by @hugomd, Check out his profile for more amazing microservices.
+
+# Requests
+| Request | Description |
+|---------|-------------|
+| `/all`  | Returns all events |
+| `/state/{state}` | Returns events within state |
+
+# LICENSE
+Refer to `LICENSE` for more informaton
---
+++ index.js
@@ -0,0 +1,66 @@
+const microApi = require('micro-api')
+const fs = require('fs-promise')
+var fileS = require('fs');
+
+function JSONHandler(file){
+  try {
+      var obj = JSON.parse(fileS.readFileSync(file, 'utf8'));
+      return obj;
+  } catch(e) {
+      return false;
+  }
+}
+
+
+const handleNone = (() => 'You must use a valid request')
+
+const handleState = async ({ params: { state }, res }) => {
+  state = state.toUpperCase()
+  try {
+    var FileObject = JSONHandler('./australianhol.json')
+    FileObject = FileObject.ausgovEvents
+    for(var i=0; i < FileObject.jurisdiction.length; i++){
+      console.log(FileObject.jurisdiction[i])
+      if(FileObject.jurisdiction[i].jurisdictionName == state){
+          return FileObject.jurisdiction[i] 
+      } else {
+          return null
+      }
+    } 
+  } catch (err) {
+    // Autos to 404
+    return null
+  }
+}
+
+const handleList = async ({ res }) => {
+  try {
+    res.setHeader('Content-Type', 'application/json')
+    const list = await fs.readFile('./australianhol.json')
+    
+    return list;
+  } catch (err) {
+    // Autos to 404
+    return null
+  }
+}
+
+const api = microApi([
+  {
+    method: 'get',
+    path: '/',
+    handler: handleNone
+  },
+  {
+    method: 'get',
+    path: '/all',
+    handler: handleList
+  },
+  {
+    method: 'get',
+    path: '/state/:state',
+    handler: handleState
+  }
+])
+
+module.exports = api
